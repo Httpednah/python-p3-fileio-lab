05503752895118b49254d52d75bb528f627e958d
Ednah wairimu kimani <kimaniednah@gmail.com>
2025-11-28 20:22:30 +0300

    lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    # Add .txt automatically when opening the file
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    # Same file, but append instead of write
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    # Read content and return it
+    try:
+        with open(f"{file_name}.txt", "r") as f:
+            return f.read()
+    except FileNotFoundError:
+        return None
